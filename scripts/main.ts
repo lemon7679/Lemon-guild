2e66ecdb5ba646fa217929c2c2eedae4cf48e124
lemon7679 <fpahs6009@gmail.com>
2024-03-10 15:59:41 +0900

    1.20.62 update

--- scripts/main.ts
+++
@@ -1,9 +0,0 @@
-import { system } from "@minecraft/server"
-import "./events/chatSend"
-import "./events/itemUse"
-import "./events/playerSpawn"
-
-system.beforeEvents.watchdogTerminate.subscribe(res => {
-    res.cancel = true
-    console.error(res.terminateReason)
-})
